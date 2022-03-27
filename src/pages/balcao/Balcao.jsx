339df48d2d7fc84409cb7ac859365ca504c8eb33
Roberta Avelino <70297387+Roberta-Oliveira@users.noreply.github.com>
2022-03-27 17:01:17 -0300

    Atualização

--- src/pages/balcao/Balcao.jsx
+++ src/pages/balcao/Balcao.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Row, Col, Card, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FcHighPriority, FcLowPriority, IconName } from "react-icons/fc";
 
 import { FaRegHandPointLeft } from "react-icons/fa";
 
@@ -17,13 +18,61 @@ const Balcao = () => {
               </Card.Header>
               <Card.Body>
                 <div>
-                  <Table striped bordered hover>
-                    <thead>
+                  <Table striped bordered hover size="sm">
+                    <tbody>
                       <tr>
-                        <tr>#</tr>
+                        <Link
+                          className="btn btn-secondary"
+                          to="/balcao/detalhes"
+                        >
+                          <td>Tempo de espera 30min</td>
+                        </Link>
+                        <td>23-03-2022</td>
+                        <td className="disaster-bg">
+                          <FcHighPriority /> Desastre
+                        </td>
                       </tr>
-                    </thead>
-                    <tbody></tbody>
+
+                      <tr>
+                        <Link to="/balcao/detalhes">
+                          <td>Atendimento mais de 30min</td>
+                        </Link>
+                        <td>23-03-2022</td>
+                        <td>
+                          <FcHighPriority /> Desastre
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <Link to="/balcao/detalhes">
+                          <td>Senhas prioritárias acima de 30min</td>
+                        </Link>
+                        <td>23-03-2022</td>
+                        <td>
+                          <FcHighPriority /> Desastre
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <Link to="/balcao/detalhes">
+                          <td>Disponibilidade do Servidor</td>
+                        </Link>
+                        <td>23-03-2022</td>
+                        <td>
+                          <FcLowPriority /> Desastre
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <Link to="/balcao/detalhes">
+                          <td>Aguadando atendimento</td>
+                        </Link>
+                        <td>23-03-2022</td>
+                        <td>
+                          <FcHighPriority /> Desastre
+                        </td>
+                      </tr>
+                    </tbody>
                   </Table>
                 </div>
               </Card.Body>
@@ -55,6 +104,7 @@ const Balcao = () => {
             </Card>
           </Col>
         </Row>
+
         <Link to="/personagens" className="btn btn-primary mt-3">
           <FaRegHandPointLeft /> Voltar
         </Link>
