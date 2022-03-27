34e4292586a08f26d679387f06926ddbe7a26061
Roberta Avelino <70297387+Roberta-Oliveira@users.noreply.github.com>
2022-03-27 15:58:21 -0300

    Atualização

--- src/pages/balcao/Balcao.jsx
+++ src/pages/balcao/Balcao.jsx
@@ -5,57 +5,60 @@ import { Link } from "react-router-dom";
 import { FaRegHandPointLeft } from "react-icons/fa";
 
 const Balcao = () => {
-  
   return (
     <>
-      <br />      
-        <>
-          <Row>
-            <Col md={5}>
-              <Card border="danger">
-                <Card.Header className="bg-danger text-white">
-                  Dados Pessoais
-                </Card.Header>
-                <Card.Body>
-                  <div>
-                   
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md={5}>
-              <Card border="danger">
-                <Card.Header className="bg-danger text-white">
-                  Ocupações
-                </Card.Header>
-                <Card.Body>
-                  <div>
-                    <Table striped bordered hover>
-                      <thead>
-                        <tr>
-                          <th>#</th>
-                          <th>Nome</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        
-                          <tr >
-                            <td></td>
-                            <td>ocupacao</td>
-                          </tr>
-                        
-                      </tbody>
-                    </Table>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-          <Link to="/personagens" className="btn btn-primary mt-3">
-            <FaRegHandPointLeft /> Voltar
-          </Link>
-        </>
-      
+      <br />
+      <>
+        <Row>
+          <Col md={5}>
+            <Card border="danger">
+              <Card.Header className="bg-danger text-white">
+                Dados Pessoais
+              </Card.Header>
+              <Card.Body>
+                <div>
+                  <Table striped bordered hover>
+                    <thead>
+                      <tr>
+                        <tr>#</tr>
+                      </tr>
+                    </thead>
+                    <tbody></tbody>
+                  </Table>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={5}>
+            <Card border="danger">
+              <Card.Header className="bg-danger text-white">
+                Ocupações
+              </Card.Header>
+              <Card.Body>
+                <div>
+                  <Table striped bordered hover>
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Nome</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td></td>
+                        <td>ocupacao</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Link to="/personagens" className="btn btn-primary mt-3">
+          <FaRegHandPointLeft /> Voltar
+        </Link>
+      </>
     </>
   );
 };
